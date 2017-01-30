8e1155e9e630ff50e1e184528f235394e7f31143
Mappy The Kat <mappythekat@naver.com>
2017-01-31 01:24:14 +0900

    주석이 사기를 치고 있네

--- OnPlaySavor.cs
+++ OnPlaySavor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEditor;
 
 /// <summary>
-/// Play 버튼 누르면 Save 해주는 유사 AutoSave Script. 추가 윈도우를 통해 기능을 켜고 끌 수 있다.(유니티 구동 시 기본값은 true)
+/// Play 버튼 누르면 Save 해주는 유사 AutoSave Script.
 /// </summary>
 
 [InitializeOnLoad]
